14fa9fefcec941bc99e1cbe0a5e6dcf9bb289edd
mervezgin <mervezgin@gmail.com>
2024-05-28 14:01:28 +0300

    added CameraAspectRatio.cs script to adjust camera for different screen ratio

--- Assets/Scripts/CameraAspectRatio.cs
+++ Assets/Scripts/CameraAspectRatio.cs
@@ -10,12 +10,6 @@ public class CameraAspectRatio : MonoBehaviour
         AdjustCamera();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void AdjustCamera()
     {
         Camera camera = Camera.main;
@@ -28,6 +22,19 @@ public class CameraAspectRatio : MonoBehaviour
             Rect rect = camera.rect;
             rect.width = 1.0f;
             rect.height = scaleHeight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleHeight) / 2.0f;
+            camera.rect = rect;
+        }
+        else
+        {
+            float scaleWidth = 1.0f / scaleHeight; 
+            Rect rect = camera.rect;
+            rect.width = scaleWidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scaleWidth) / 2.0f;
+            rect.y = 0;
+            camera.rect = rect;
         }
     }
 }
