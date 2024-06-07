4dd756d2187e909fb9e513138397047e914bc565
mervezgin <mervezgin@gmail.com>
2024-06-07 16:22:36 +0300

    added comment to HeadLevelShow.cs script

--- Assets/Scripts/HeadLevelShow.cs
+++ Assets/Scripts/HeadLevelShow.cs
@@ -5,19 +5,19 @@ using UnityEngine.UI;
 
 public class HeadLevelShow : MonoBehaviour
 {
-    [SerializeField] Transform characterTransform;
-    [SerializeField] Vector3 offset;
-    [SerializeField] Text levelText;
+    [SerializeField] Transform characterTransform; //Karakterin world spaceteki pozisyonunu rotasyonunu ve ölçüsünü belirtir.
+    [SerializeField] Vector3 offset; //Karaketerin başının üzerindeki text konumunu ayarlamak için.
+    [SerializeField] Text levelText; // Karakterin başının üzerindeki UI text objesi.
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 screenPosition = Camera.main.WorldToScreenPoint(characterTransform.position + offset);
-        levelText.transform.position = screenPosition;
+        Vector3 screenPosition = Camera.main.WorldToScreenPoint(characterTransform.position + offset); //characterTransforma offset eklenir. ve bu world spaceteki konum, screen spaceteki konuma dönüştürülür.
+        levelText.transform.position = screenPosition; //levelText doğru yere konumlandırılır.
     }
 
     public void SetLevel(int level)
     {
-        levelText.text = "Level: " + level;
+        levelText.text = "LVL. " + level; // Karakterin başının üzerindeki konum yazılır.
     }
 }
