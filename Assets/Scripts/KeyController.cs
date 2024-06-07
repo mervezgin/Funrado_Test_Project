c04bf0f87bab75eed131fd253a8e56882fb272f5
mervezgin <mervezgin@gmail.com>
2024-06-07 14:35:47 +0300

    added comment to KeyController.cs script

--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -4,24 +4,22 @@ using UnityEngine;
 
 public class KeyController : MonoBehaviour
 {
-    public bool hasRedKey = false;
-    public bool hasBlueKey = false;
+    public bool hasRedKey = false; //Karakterin RedKey'e sahip olup olmadığını belirten değişken
+    public bool hasBlueKey = false; //Karakterin BlueKey'e sahip olup olmadığını belirten değişken
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other) //RedKey ve BlueKey BoxCollider component'ında isTrigger seçili olmalı
     {
-        if (other.gameObject.CompareTag("BlueKey"))
+        if (other.gameObject.CompareTag("BlueKey")) //BlueKey tagi olan anahtarla triggerlandığında
         {
             hasBlueKey = true;
-            Destroy(other.gameObject);
+            Destroy(other.gameObject); //BlueKey'i yok eder
             Debug.Log("you can open blue door bro");
-            //open the blue door with oscillation
         }
-        else if (other.gameObject.CompareTag("RedKey"))
+        else if (other.gameObject.CompareTag("RedKey")) //RedKey tagi olan anahtarla triggerlandığında
         {
             hasRedKey = true;
-            Destroy(other.gameObject);
+            Destroy(other.gameObject); //RedKey'i yok eder
             Debug.Log("you can open red door bro");
-            //open the red door with oscillation
         }
     }
 }
