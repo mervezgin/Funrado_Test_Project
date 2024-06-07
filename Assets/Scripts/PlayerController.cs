56cb54c258fce18bfd3a1c9e082b6e17c2008b96
mervezgin <mervezgin@gmail.com>
2024-06-07 14:25:46 +0300

    added comment to PlayerController.cs script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,46 +18,43 @@ public class PlayerController : MonoBehaviour
         animator = GetComponent<Animator>();
         joystick = FindObjectOfType<Joystick>();
 
-        playerRb.freezeRotation = true;
+        playerRb.freezeRotation = true; //Rigidbody'nin dönüşünü durdurur, bu sayede sadece kodla dönüş sağlayabiliriz. 
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*#if UNITY_EDITOR
-                KeyboardControl();
-        #else*/
+        //KeyboardControl();
         JoystickControl();
-        /*#endif*/
     }
 
     public void KeyboardControl()
     {
-        float moveDirection = Input.GetAxis("Vertical");
-        float rotateDirection = Input.GetAxis("Horizontal");
+        float moveDirection = Input.GetAxis("Vertical"); // W, S ya da yukarı ok tuşu, aşağı ok tuşu ile ileri geri hareket girdisi
+        float rotateDirection = Input.GetAxis("Horizontal"); // A, D ya da sağ ok tuşu, sol ok tuşu ile sağa sola hareket girdisi
 
-        Vector3 playerMove = transform.forward * moveDirection * moveSpeed * Time.deltaTime;
-        playerRb.MovePosition(playerRb.position + playerMove);
+        Vector3 playerMove = transform.forward * moveDirection * moveSpeed * Time.deltaTime; // Hareket vektörünü hesaplar.
+        playerRb.MovePosition(playerRb.position + playerMove); //Rigidbody'yi yeni pozisyona hareket ettirir.
 
-        float rotation = rotateDirection * rotationSpeed * Time.deltaTime;
-        transform.Rotate(0, rotation, 0);
+        float rotation = rotateDirection * rotationSpeed * Time.deltaTime; // Dönüş açısını hesaplar.
+        transform.Rotate(0, rotation, 0); //Karakteri döndürür.
 
-        bool isRunning = moveDirection != 0 || rotateDirection != 0;
-        animator.SetBool("isRunning", isRunning);
+        bool isRunning = moveDirection != 0 || rotateDirection != 0; //Karakterin hareket edip etmediğini kontrol eder.
+        animator.SetBool("isRunning", isRunning); //Animasyon parametresini günceller.
     }
 
     public void JoystickControl()
     {
-        float moveDirection = joystick.Vertical;
-        float rotateDirection = joystick.Horizontal;
+        float moveDirection = joystick.Vertical; //Joystick dikey eksen girdisi
+        float rotateDirection = joystick.Horizontal; //Joystick yatay eksen girdisi
 
-        Vector3 playerMove = transform.forward * moveDirection * moveSpeed * Time.deltaTime;
-        playerRb.MovePosition(playerRb.position + playerMove);
+        Vector3 playerMove = transform.forward * moveDirection * moveSpeed * Time.deltaTime; // Hareket vektörünü hesaplar.
+        playerRb.MovePosition(playerRb.position + playerMove); //Rigidbody'yi yeni pozisyona hareket ettirir.
 
-        float rotation = rotateDirection * rotationSpeed * Time.deltaTime;
-        transform.Rotate(0, rotation, 0);
+        float rotation = rotateDirection * rotationSpeed * Time.deltaTime;// Dönüş açısını hesaplar.         
+        transform.Rotate(0, rotation, 0); //Karakteri döndürür.
 
-        bool isRunning = moveDirection != 0 || rotateDirection != 0;
-        animator.SetBool("isRunning", isRunning);
+        bool isRunning = moveDirection != 0 || rotateDirection != 0; //Karakterin hareket edip etmediğini kontrol eder.
+        animator.SetBool("isRunning", isRunning); //Animasyon parametresini günceller.
     }
 }
